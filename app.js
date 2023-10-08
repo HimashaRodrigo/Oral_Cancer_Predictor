454fc435e2c9d1152bf69db5593994248cee3f0b
unknown <himasharodrigo1998@gmail.com>
2023-10-08 20:01:01 +0530

    add initial files and created frontend

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/diagnosis", (req, res) => {
+  res.render("diagnosis");
+});
+
+app.get("/consultancy", (req, res) => {
+  res.render("consultancy");
+});
+
+app.get("/doctors", (req, res) => {
+  res.render("doctors");
+});
+
+app.get("/predict", (req, res) => {
+  res.render("predict");
+});
+
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
